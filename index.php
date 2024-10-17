8d3a65a6b4458ef0e675fbd23d02940d4a19c8be
hanine007 <bouchibahanine@gmail.com>
2024-10-18 00:52:40 +0100

    add a style link

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+<link rel="stylesheet" href="style.css">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crypto Convert</title>
 </head>
 <body>
+<!-- action is post for not showiing in the url action is where the data is will going -->
     <form method="post" action="Convert.php">
         <label >Amount</label>
         <input type="number" name="amount" id="amount">
