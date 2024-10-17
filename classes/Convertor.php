b7a58e852714c30dfc4c7faca3a8fb68b9a0406a
hanine007 <bouchibahanine@gmail.com>
2024-10-18 00:52:20 +0100

    class and methods

---
+++ classes/Convertor.php
@@ -0,0 +1,26 @@
+<?php 
+Class Crypto {
+
+}
+Class Convert extends Crypto{
+    //proprties
+    public  $Cuurent;
+    // constructeur
+    public function __construct($Cuurent){
+        $this->Cuurent=$Cuurent;// construct for the class is caled when we create a object 
+    }
+    //methods
+    public function convert ($value){
+        $code=$this->Cuurent;//get the crypto selected in the object created
+        $url=("https://cex.io/api/ticker/$code/USD");// the url of the api
+        $json=file_get_contents($url); //get the content 
+       echo"$json";
+        $data=json_decode($json);//get only the last  info in the json 
+        $bitcoinvalue=$data->last;
+       return $value*$bitcoinvalue; //the result that we will return 
+    
+    }
+
+
+}
+//cretae object
