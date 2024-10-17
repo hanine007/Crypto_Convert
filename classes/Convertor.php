8e0325ea1a3d87e72d9c1c6c575eb3f9c3e10c4b
hanine007 <bouchibahanine@gmail.com>
2024-10-18 00:59:35 +0100

    add a commit

--- classes/Convertor.php
+++ classes/Convertor.php
@@ -7,11 +7,11 @@ Class Convert extends Crypto{
     public  $Cuurent;
     // constructeur
     public function __construct($Cuurent){
-        $this->Cuurent=$Cuurent;// construct for the class is caled when we create a object 
+        $this->Cuurent=$Cuurent;// construct for the class is caled automaticly  when we create a object 
     }
     //methods
     public function convert ($value){
-        $code=$this->Cuurent;//get the crypto selected in the object created
+        $code=$this->Cuurent;//get the crypto selected in the object created get the prop of the object 
         $url=("https://cex.io/api/ticker/$code/USD");// the url of the api
         $json=file_get_contents($url); //get the content 
        echo"$json";
